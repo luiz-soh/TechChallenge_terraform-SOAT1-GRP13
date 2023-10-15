f00b8314f50275dc616be74d6604c7177c45d408
luiz-soh <souza_luiz@live.com>
2023-10-15 18:20:40 -0300

    first commit

---
+++ modules/networking/variables.tf
@@ -0,0 +1,27 @@
+variable "environment" {
+  description = "Ambiente de desenvolvimento"
+  default = "dev"
+}
+variable "vpc_cidr" {
+  description = "cidr"
+  default = "10.0.0.0/16"
+}
+variable "availability_zones" {
+  type        = list
+  description = "availability zone para as subnets"
+  default = ["us-west-2a","us-west-2b"]
+}
+variable "public_subnets_cidr" {
+  type        = list
+  description = "public subnet"
+  default = ["10.0.0.0/17"]
+}
+variable "private_subnets_cidr" {
+  type        = list
+  description = "private subnet"
+  default = ["10.0.128.0/17"]
+}
+variable "region" {
+  description = "Ambiente de desenvolvimento"
+  default = "dev"
+}
